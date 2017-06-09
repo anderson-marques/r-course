bb95edd1a2ecc97c6b183cc875ddf89cb0c058af
anderson.mo.carvalho <anderson.mo.carvalho@gmail.com>
2017-06-08 21:56:32 -0300

    First file added

---
+++ beginning.R
@@ -0,0 +1,21 @@
+# Learning R Language
+
+# ctrl + Enter to execute (Can select what to run )
+
+2 + 1 # Basic math
+
+1:200 # iteration
+
+print("Hello World")
+
+x <- 1:3 # assign
+x # display the value of x
+
+y <- c(6,7,8,9,11,12) # concacnate numbers
+
+y
+
+x + y # for each in x + the corresponding y
+x * y # for each in x * the corresponding y
+
+ls() # list values
